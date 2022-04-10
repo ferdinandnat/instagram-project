3eeefd4f7cf8bc6c3d53d35d1b43fd442d56c3cf
Ferdinand Natanael <ferdinand.natanael20@gmail.com>
2022-04-10 15:56:08 +0700

    instagram-project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Home from "./pages/home"
+import Profile from "./pages/profile"
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Instagram</h1>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+        
+    </Routes>
     </div>
   );
 }
---
+++ src/components/home/feeds/index.js
@@ -0,0 +1,12 @@
+import {useEffect} from 'react'
+
+function Feeds() {
+
+(<div>
+    Feeds
+</div>)
+return
+
+}
+
+export default Feeds
