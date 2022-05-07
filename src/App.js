36c0d4835b955eccafde3a60cab45fdaa6304927
Ferdinand Natanael <ferdinand.natanael20@gmail.com>
2022-05-07 15:11:50 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from "./pages/home"
 import Profile from "./pages/profile"
+import Post from "./pages/post-create"
 import {Routes,Route} from 'react-router-dom'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/profile' element={<Profile/>}/>
+      <Route path='/post-create' element={<Post/>}/>
         
     </Routes>
     </div>
