922c51cde2dd038e4c46f7993a5a74a4990820bc
Ferdinand Natanael <ferdinand.natanael20@gmail.com>
2022-05-07 16:52:11 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from "./pages/home"
 import Profile from "./pages/profile"
 import Post from "./pages/post-create"
+import Feed from "./pages/feed-detail"
 import {Routes,Route} from 'react-router-dom'
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/profile' element={<Profile/>}/>
       <Route path='/post-create' element={<Post/>}/>
-        
+      <Route path='/feed-detail' element={<Feed/>}/>
     </Routes>
     </div>
   );
